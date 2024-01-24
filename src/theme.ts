77930a34fd80bac73d7eb5b2493e761d9753c2d6
yeguu <qsc130@gmail.com>
2024-01-24 19:07:02 +0900

    #7.0_Get_Selectors

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "#2668ed",
+  cardBgColor: "#061b36",
+  subTextColor: "#a9b1ba",
+};
