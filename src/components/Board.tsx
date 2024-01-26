1210869b131d566e397a2830769eeea0cf76c0d1
yeguu <qsc130@gmail.com>
2024-01-26 16:13:05 +0900

    #7.13_Refs

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,7 @@
 import { Droppable } from "react-beautiful-dnd";
 import styled from "styled-components";
 import DraggableCard from "./DraggableCard";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   width: 300px;
@@ -42,9 +43,22 @@ const Area = styled.div<IAreaProps>`
 `;
 
 const Board = ({ toDos, boardId }: IBoardProps) => {
+  // input element에 대한 ref 생성
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // input element를 포커싱하고 5초 후에 블러 처리하는 함수
+  const onClick = () => {
+    inputRef.current?.focus();
+    setTimeout(() => {
+      inputRef.current?.blur();
+    }, 5000);
+  };
+
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      <input ref={inputRef} placeholder="grab me" />
+      <button onClick={onClick}>click me</button>
       <Droppable droppableId={boardId}>
         {(provided, snapshot) => (
           <Area
@@ -79,4 +93,18 @@ export default Board;
 
 - isUsingPlaceholder: boolean
   - placeholder가 사용되고 있는지 여부
+*/
+
+/* useRef()
+
+- .current 프로퍼티로 전달된 인자(initalValue)로 초기화된 변경 가능한 ref를 반환함
+- 반환된 객체는 컴포넌트의 전 생명주기(life cycle) 동안 유지됨
+- 일반적인 사용 사례는 children에 접근하는 경우이며, 이를 통해 DOM 요소나 다른 컴포넌트에 접근할 수 있음
+- useRef는 함수 컴포넌트에서 변경 가능한 상태를 유지할 수 있는 유일한 방법이며, 렌더링과 관계없이 값이 보존됨
+- 주로 이전 상태 또는 프로퍼티 값을 저장하고 싶을 때 활용됨
+
+- ref 속성 대신 useRef()를 사용하는 것이 더 유용함
+- 클래스 컴포넌트에서는 인스턴스 필드를 사용하여 상태를 관리하지만, 함수 컴포넌트에서는 useRef를 사용하여 유지되는 값을 갖게 됨
+
+- 주의: useRef를 사용하여 DOM에 직접 접근하는 것은 피해야 하며, 이런 경우에는 useEffect와 함께 활용하는 것이 좋음
 */
