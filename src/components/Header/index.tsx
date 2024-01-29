2be4d94b5309819145425d3485d697885e50afac
yeguu <qsc130@gmail.com>
2024-01-30 01:29:46 +0900

    feat: 새로운 board 생성 로직 구현 및 board 생성 관련 스타일 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,31 @@
+import { useForm } from "react-hook-form";
 import * as style from "./styles";
+import { useSetRecoilState } from "recoil";
+import { IBoard, boardState } from "../../atoms";
+
+interface IBoardForm {
+  board: string;
+}
 
 const Header = () => {
+  const setBoard = useSetRecoilState(boardState);
+  const { register, setValue, handleSubmit } = useForm<IBoardForm>();
+
+  const onValid = ({ board }: IBoardForm) => {
+    // 새로운 board 생성
+    const newBoard: IBoard = {
+      title: board,
+      content: [],
+    };
+
+    setBoard((currnetBoards) => {
+      return [...currnetBoards, newBoard];
+    });
+
+    // 입력 필드 초기화
+    setValue("board", "");
+  };
+
   return (
     <style.HeaderContainer>
       <hr />
@@ -8,12 +33,13 @@ const Header = () => {
       <hr />
       <style.SubWrapper>
         <style.SubTitle>Create a new board 👉</style.SubTitle>
-        <style.BoardCreateForm>
+        <style.BoardForm onSubmit={handleSubmit(onValid)}>
           <input
+            {...register("board", { required: true })}
             type="text"
             placeholder={`Type board name here and press enter to create`}
           />
-        </style.BoardCreateForm>
+        </style.BoardForm>
       </style.SubWrapper>
       <hr />
     </style.HeaderContainer>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -11,26 +11,33 @@ export const HeaderContainer = styled.div`
 export const Title = styled.div`
   font-size: 40px;
   font-weight: 700;
-  padding-left: 6px;
+  padding: 0px 6px;
 `;
 
 export const SubWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 10px;
+  gap: 6px;
+  padding: 0px 6px;
 `;
 
 export const SubTitle = styled.div`
   font-size: 20px;
   font-weight: 700;
-  padding-left: 6px;
 `;
 
-export const BoardCreateForm = styled.form`
+export const BoardForm = styled.form`
   flex: 1;
   input {
     width: 100%;
     padding: 6px;
-    box-sizing: border-box;
+    border: none;
+    outline: none;
+    background-color: #dddddd;
+    font-size: 16px;
+    font-weight: 500;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
   }
 `;
