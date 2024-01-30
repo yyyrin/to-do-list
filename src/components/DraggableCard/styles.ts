171fe44b53e6ac7e41e48757aaedeb380c801ea4
yeguu <qsc130@gmail.com>
2024-01-30 23:57:35 +0900

    feat: toDo 수정 구현을 위한 스타일링

--- src/components/DraggableCard/styles.ts
+++ src/components/DraggableCard/styles.ts
@@ -1,22 +1,26 @@
 import styled, { css } from "styled-components";
 import DeleteIc from "../../assets/delteIc.svg?react";
 import EditIc from "../../assets/editIc.svg?react";
+import FilledDeleteIc from "../../assets/filledDeleteIc.svg?react";
 
-export const Card = styled.div<{ $isDragging: boolean }>`
+export const Card = styled.div<{ $isDragging: boolean; $isEdit: boolean }>`
+  height: 58px;
+  border: 2px solid black;
   border-radius: 4px;
   padding: 14px 12px;
   margin-bottom: 8px;
   background-color: ${(props) =>
-    props.$isDragging ? "rgba(0, 0, 0, 0.5)" : props.theme.cardColor};
+    props.$isEdit ? "white" : props.theme.cardColor};
   box-shadow: ${(props) =>
     props.$isDragging ? "0px 2px 5px rgba(0, 0, 0, 0.05)" : "none"};
   display: flex;
   justify-content: space-between;
   align-items: center;
   gap: 4px;
+  opacity: ${(props) => (props.$isDragging ? 0.7 : 1)};
 
   p {
-    color: white;
+    color: ${(props) => (props.$isEdit ? "black" : "white")};
     font-size: 14px;
     font-weight: 500;
     text-overflow: ellipsis;
@@ -49,4 +53,31 @@ export const DeleteIcStyle = styled(DeleteIc)`
 
 export const EditIcStyle = styled(EditIc)`
   ${IconStyle}
+`;
+
+export const EditForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4px;
+
+  input {
+    width: 100%;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    font-size: 14px;
+    font-weight: 500;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+`;
+
+export const EditCancelBtn = styled(FilledDeleteIc)`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  flex-shrink: 0;
 `;
