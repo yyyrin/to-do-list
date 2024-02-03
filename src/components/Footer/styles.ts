1800103fd680ef8bb911620e968b1a37cf8bdd61
yeguu <qsc130@gmail.com>
2024-02-03 18:36:59 +0900

    feat: Footer 컴포넌트 추가 및 AppContainer width를 100%로 수정

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 0;
+  margin: 80px auto 20px auto;
+  font-size: 14px;
+  font-weight: 500;
+  color: #9d9e9d;
+`;
